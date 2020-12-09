5e0776ac7641f53e9ce2f9a3ee7ffc309d1a6d80
Ken Smith <ken@ivisibility.com>
2020-12-08 19:05:37 -0600

    Add project files.

---
+++ CloudDiags/Controllers/NetworkController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+
+
+namespace CloudDiags.Controllers
+{
+    public class NetworkController : Controller
+    {
+        public IActionResult Index()
+        {
+            string anAddress = "google.com";
+            PingIt p = new PingIt();
+
+            String strReturn = string.Empty;
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send(anAddress, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                ViewData["Address"] = reply.Address.ToString();
+            }
+
+            return View();
+        }
+
+        public ActionResult Vault()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+
+        }
+
+        public string Ping(string name)
+        {
+            PingIt p = new PingIt();
+            return HtmlEncoder.Default.Encode($"Ping {p.PingItStr("ivisibility.com")}");
+
+        }
+
+    }
+}
+
---
+++ CloudDiags/PingIt.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+
+
+namespace CloudDiags.Controllers
+{
+    public class PingIt
+    {
+        public string m_sAddress=string.Empty;
+
+        public PingIt()
+        {
+
+        }
+
+        // public static void Main(string[] args)
+        public PingIt(string anAddress)
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send(anAddress, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+            }
+        }
+
+        public string PingItStr(string anAddress)
+        {
+            String strReturn = string.Empty;
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send(anAddress, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+
+                strReturn += ("Address: {0}", reply.Address.ToString());
+                strReturn += ("\nRoundTrip time: {0}", reply.RoundtripTime);
+                strReturn += ("\nRoundTrip time: {0}", reply.RoundtripTime);
+
+                /*Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+                */
+            }
+            return strReturn;
+        }
+
+    }
+}
